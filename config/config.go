ade11632ab31e7ba0f41b259b7e87441bc613e73
Yansong Wang <w1079057544@gmail.com>
2024-11-28 00:36:03 -0500

    improve database connection settings

--- config/config.go
+++ config/config.go
@@ -2,30 +2,33 @@ package config
 
 import (
 	"fmt"
-	"log"
-	"os"
-
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
-// 初始化数据库连接
 func InitDB() {
-	// 从环境变量中获取配置
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	// Get Configuration
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	// 构造 MySQL DSN
+	// Constructing DSN
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser, dbPass, dbHost, dbPort, dbName)
 
-	// 连接到数据库
-	var err error
+	// Connect to the database
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
 module ecommerce-api
 
-go 1.19
+go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v4 v4.5.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.29.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
