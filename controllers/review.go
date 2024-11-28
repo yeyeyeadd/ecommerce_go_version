a6be063673a3f24bf4e4382cc88b8091aba25119
Yansong Wang <w1079057544@gmail.com>
2024-11-27 23:12:02 -0500

    fixd review: add comment

--- controllers/review.go
+++ controllers/review.go
@@ -10,10 +10,11 @@ import (
 // Add Comment
 func CreateReview(c *gin.Context) {
 	var input struct {
-		UserID    uint `json:"user_id" binding:"required"`
-		ProductID uint `json:"product_id" binding:"required"`
-		OrderID   uint `json:"order_id" binding:"required"`
-		Rating    int  `json:"rating" binding:"required,min=1,max=5"`
+		UserID    uint   `json:"user_id" binding:"required"`
+		ProductID uint   `json:"product_id" binding:"required"`
+		OrderID   uint   `json:"order_id" binding:"required"`
+		Rating    int    `json:"rating" binding:"required,min=1,max=5"`
+		Comment   string `json:"comment"`
 	}
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -25,6 +26,7 @@ func CreateReview(c *gin.Context) {
 		ProductID: input.ProductID,
 		OrderID:   input.OrderID,
 		Rating:    input.Rating,
+		Comment:   input.Comment,
 	}
 	if err := models.DB.Create(&review).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
